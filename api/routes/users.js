3c62be60b1cbef72b6f7cda8583a742f16e68b10
Ece Polat <ece_polat1409@hotmail.com>
2026-01-21 00:13:44 +0300

    Authentication

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const bcrypt = require("bcrypt-nodejs");
 const is = require("is_js");
+const jwt = require("jwt-simple");
 
 const Response = require('../lib/Response');
 const Users = require('../db/models/Users');
@@ -9,6 +10,7 @@ const Enum = require('../config/Enum');
 var router = express.Router();
 const UserRoles = require("../db/models/UserRoles");
 const Roles = require("../db/models/Roles");
+const config = require('../config');
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
@@ -190,6 +192,40 @@ router.post("/register", async (req, res) => {
     });
 
     res.status(Enum.HTTP_CODES.CREATED).json(Response.successResponse({success: true}, Enum.HTTP_CODES.CREATED));
+
+  } catch(err){
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/auth", async(req, res) => {
+  try{
+
+    let {email, password} = req.body;
+
+    Users.validateFieldsBeforeAuth(email, password);
+
+    let user = await Users.findOne({email});
+
+    if(!user) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error!", "Email or password wrong");
+
+    if(!user.validPassword(password)) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error!", "Email or password wrong");
+
+    let payload = {
+      id: user._id,
+      exp: parseInt(Date.now() / 1000) * config.JWT.EXPIRE_TIME
+    }
+
+    let token = jwt.encode(payload, config.JWT.SECRET);
+
+    let userData = {
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name
+    }
+
+    res.json(Response.successResponse({token, user: userData}));
 
   } catch(err){
     let errorResponse = Response.errorResponse(err);
